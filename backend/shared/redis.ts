c80dd99617559f5bf7d70d6a5966ceddddca8ee6
Saksham <saksham1387@gmail.com>
2026-05-05 14:34:37 +0530

    added deployment files

--- backend/shared/redis.ts
+++ backend/shared/redis.ts
@@ -1,7 +1,9 @@
 import { createClient } from "redis";
 
-export const publisher  = createClient();
-export const subscriber = createClient();
+const redisUrl = process.env.REDIS_URL;
+
+export const publisher  = createClient({ url: redisUrl });
+export const subscriber = createClient({ url: redisUrl });
 
 await publisher.connect();
 await subscriber.connect();
